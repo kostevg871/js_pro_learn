a65e32f449dd187479fc3906ab6ecd9067c1c4b9
kosteg <kostyrkoevgeniy@yandex.ru>
2023-01-18 22:06:10 +0300

    ls 1.17 sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
+import { Main } from 'Containers/Main';
 import React from 'react';
 import './App.css';
-import { CounterComponent } from './CounterComponent';
-import { Input } from './input';
 
 function App() {
-  const handler = () => {
-    console.log('Кнопка была нажата');
-  };
-  return (
-    <div className="App">
-      <CounterComponent title={'Апельсин'} count={8} />
-      <CounterComponent title={'Арбуз'} count={4} />
-      <CounterComponent title={'Мандаринки'} count={20} />
-      <div onClickCapture={() => console.log('div')}>
-        <p onClickCapture={() => console.log('p')}>
-          <button onClick={handler}>PUSH ME</button>
-        </p>
-      </div>
-      <Input />
-
-      <img src={'/img.jfif'} alt="image" />
-    </div>
-  );
+  return <Main />;
 }
 
 export default App;
---
+++ src/Components/Common/Header/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import style from './Header.module.css';
+
+export const Header = () => {
+  return <header className={style.header}>
+    <span className={style.logo}>LOGO</span>
+    <span className={style.title}>APP_NAME</span>
+    </header>;
+};
