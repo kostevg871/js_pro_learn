b183a74b1b71d157928e12e082bba0b1507c3295
kosteg <kostyrkoevgeniy@yandex.ru>
2023-01-19 22:18:55 +0300

    routes, ls 2.30

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 // import { Main } from 'Containers/Main';
+import { Main } from 'Containers/Main';
 import { Posts } from 'Containers/Posts';
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return <Posts />;
+  return (
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/posts" element={<Posts />} />
+    </Routes>
+  );
 }
 
 export default App;
