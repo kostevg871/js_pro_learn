c201854fb41a2babc02ea33498aca63bfca064d8
kosteg <kostyrkoevgeniy@yandex.ru>
2023-01-20 20:18:43 +0300

    notFound add

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Main } from 'Containers/Main';
 import { Posts } from 'Containers/Posts';
 import { Route, Routes } from 'react-router-dom';
 import { routes } from 'Helpers/Constants/roustes';
+import { NotFound } from 'Containers/NotFound';
 
 function App() {
   return (
     <Routes>
       <Route path={routes.main} element={<Main />} />
       <Route path={routes.posts} element={<Posts />} />
+      <Route path={routes.notFound} element={<NotFound />} />
     </Routes>
   );
 }
